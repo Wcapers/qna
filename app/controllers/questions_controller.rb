1d2af1e8a569b3f4bbd1be063240193b195442c4
Wcapers <Mirazizovt@yandex.ru>
2020-10-21 20:40:31 +0300

    add question create feature

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,35 +1,51 @@
 class QuestionsController < ApplicationController
-    before_action :set_question, only: %i[show]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :load_question, only: [:show, :edit, :update, :destroy]
 
-    def index
-      @questions = Question.all
+  def index
+    @questions = Question.all
+  end
+
+  def show
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def edit
+  end
+
+  def create
+    @question = Question.new(question_params)
+
+    if @question.save
+      redirect_to @question, notice: 'Your question successfully created.'
+    else
+      render :new
     end
+  end
 
-    def show
+  def update
+    if @question.update(question_params)
+      redirect_to @question
+    else
+      render :edit
     end
+  end
 
-    def new
-    end
+  def destroy
+    @question.destroy
+    redirect_to questions_path
+  end
 
-    def create
-      @question = Question.new(question_params)
+  private
 
-      if @question.save
-        redirect_to @question
-      else
-        render :new
-      end
-    end
+  def load_question
+    @question = Question.find(params[:id])
+  end
 
-    private
-
-    def set_question
-      @question = Question.find(params[:id])
-    end
-
-    helper_method :question
-
-    def question_params
-      params.require(:question).permit(:title, :body)
-    end
+  def question_params
+    params.require(:question).permit(:title, :body)
+  end
 end
